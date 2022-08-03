15b5eabb9b1c503b4936a6ad35f43bbab91774c6
Maksim Paskal <paskal.maksim@gmail.com>
2022-08-03 06:49:32 +0300

    add metrics
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- hcloud/cloud.go
+++ hcloud/cloud.go
@@ -26,6 +26,7 @@ import (
 	"strings"
 
 	"github.com/hetznercloud/hcloud-cloud-controller-manager/internal/hcops"
+	"github.com/hetznercloud/hcloud-cloud-controller-manager/internal/metrics"
 	"github.com/hetznercloud/hcloud-go/hcloud"
 	"github.com/hetznercloud/hcloud-go/hcloud/metadata"
 	cloudprovider "k8s.io/cloud-provider"
@@ -47,6 +48,8 @@ const (
 	hcloudLoadBalancersDisablePrivateIngress = "HCLOUD_LOAD_BALANCERS_DISABLE_PRIVATE_INGRESS"
 	hcloudLoadBalancersUsePrivateIP          = "HCLOUD_LOAD_BALANCERS_USE_PRIVATE_IP"
 	hcloudLoadBalancersDisableIPv6           = "HCLOUD_LOAD_BALANCERS_DISABLE_IPV6"
+	hcloudMetricsEnabledENVVar               = "HCLOUD_METRICS_ENABLED"
+	hcloudMetricsAddress                     = ":8233"
 	nodeNameENVVar                           = "NODE_NAME"
 	providerName                             = "hcloud"
 	providerVersion                          = "v1.9.1"
@@ -63,6 +66,7 @@ type cloud struct {
 
 func newCloud(config io.Reader) (cloudprovider.Interface, error) {
 	const op = "hcloud/newCloud"
+	metrics.OperationCalled.WithLabelValues(op).Inc()
 
 	token := os.Getenv(hcloudTokenENVVar)
 	if token == "" {
@@ -80,6 +84,14 @@ func newCloud(config io.Reader) (cloudprovider.Interface, error) {
 		hcloud.WithToken(token),
 		hcloud.WithApplication("hcloud-cloud-controller", providerVersion),
 	}
+
+	// start metrics server if enabled (enabled by default)
+	if os.Getenv(hcloudMetricsEnabledENVVar) != "false" {
+		go metrics.Serve(hcloudMetricsAddress)
+
+		opts = append(opts, hcloud.WithInstrumentation(metrics.GetRegistry()))
+	}
+
 	if os.Getenv(hcloudDebugENVVar) == "true" {
 		opts = append(opts, hcloud.WithDebugWriter(os.Stderr))
 	}
@@ -245,6 +257,8 @@ func loadBalancerDefaultsFromEnv() (hcops.LoadBalancerDefaults, bool, bool, erro
 // network.
 func serverIsAttachedToNetwork(metadataClient *metadata.Client, networkID int) (bool, error) {
 	const op = "serverIsAttachedToNetwork"
+	metrics.OperationCalled.WithLabelValues(op).Inc()
+
 	serverPrivateNetworks, err := metadataClient.PrivateNetworks()
 	if err != nil {
 		return false, fmt.Errorf("%s: %s", op, err)
